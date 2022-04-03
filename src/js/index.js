b274aa589a9c5957b440f8911bce015d3117ee69
Karl Podger <karl.podger@primeordinal.com>
2022-04-03 21:58:17 +0100

    Add output to screen

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,3 @@ import logger from './logger';
 import '../css/index.scss';
 
 logger('it works well!');
-
-import { getRandomGesture, makeGesture, rounds } from './logic.js';
-
-
-while (rounds.length < 50) {
-    makeGesture(getRandomGesture);
-}
-
-console.log({rounds});
--- src/js/logic.js
+++ src/js/logic.js
@@ -1,8 +1,44 @@
-// export const rules = {
-//     rock: ['scissors'],
-//     paper: ['rock'],
-//     scissors: ['paper'],
-// };
+export const rounds = [];
+
+export const getRoundDetails = (gestures, rules) => {
+	let winner = 'draw';
+
+    if (rules[gestures?.player1]?.includes(gestures?.player2)) {
+        winner = 'Player 1';
+    }
+
+    if (rules[gestures?.player2]?.includes(gestures?.player1)) {
+        winner = 'Player 2';
+    }
+
+    const details = Object.freeze({
+        gestures,
+        winner,
+    });
+
+    return details;
+}
+
+export const makeGesture = (playerGesture, rules = {
+    rock: ['scissors'],
+    paper: ['rock'],
+    scissors: ['paper'],
+}) => {
+	const index = Math.floor(Math.random() * Object.keys(rules).length + 1);
+
+	const getRandomGesture = Object.keys(rules)[index];
+
+	const gestures = {
+		player1: playerGesture,
+		player2: getRandomGesture,
+	};
+
+	const details = getRoundDetails(gestures, rules);
+
+    rounds.push(details);
+
+	return details;
+};
 
 export const rules = {
     rock: ['scissors', 'lizard'],
@@ -12,33 +48,17 @@ export const rules = {
     spock: ['rock', 'scissors'],
 };
 
-export const rounds = [];
+const buttons = document.querySelectorAll('menu button');
+const main = document.querySelector('main');
 
-export const getRoundDetails = (player1Gesture, player2Gesture) => {
-    let winner = 'draw';
+buttons.forEach(button => button.addEventListener('click', () => {
+	const playerGesture = button.dataset.gesture;
 
-    if (rules[player1Gesture].includes(player2Gesture)) {
-        winner = 'Player 1';
-    }
+	const details = makeGesture(playerGesture);
 
-    if (rules[player2Gesture].includes(player1Gesture)) {
-        winner = 'Player 2';
-    }
-
-    const details = Object.freeze({
-        player1Gesture,
-        player2Gesture,
-        winner,
-    });
-
-    return details;
-}
-
-export const getRandomGesture = Object.keys(rules)[Math.floor(Math.random() * 5)];
-
-export const makeGesture = () => {
-    const details = getRoundDetails(getRandomGesture, getRandomGesture);
-
-    rounds.push(details);
-};
-
+	main.innerHTML = `
+		<div>Winner: ${details.winner}</div>
+		<div>Player 1: ${rounds?.filter(round => round.winner === 'Player 1')?.length}</div>
+		<div>Player 2: ${rounds?.filter(round => round.winner === 'Player 2')?.length}</div>
+	`;
+}));
